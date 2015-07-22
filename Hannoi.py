46e3bccb031c421286ec7afeb8e3ca493d791f4e
Rodrigotanure <rodrigottricarico@gmail.com>
2015-07-22 18:38:02 -0300

    Update Hannoi.py

--- Hannoi.py
+++ Hannoi.py
@@ -1,5 +1,7 @@
 from mauaserver import *
 
+#Funcao hannoi com contador
+
 def hannoi(n, org, dest, temp, count=0):
     if n == 1:
         count += 1
@@ -9,14 +11,44 @@ def hannoi(n, org, dest, temp, count=0):
         count = hannoi(1, org, dest, temp, count)
         count = hannoi(n-1, temp, dest, org, count)
     return count
-a={}
 
+#Numero de testes
 
-b=hannoi(3,1,2,3)
+while True:
+    try:
+        l = int(raw_input("Digite quantos testes do hannoi deseja realizar: "))
+        if (l>0 and l<=10):
+            break
+        else:
+            print("Digite um numero maior que 0 e menor que 10")
+    except ValueError:
+        print "Digite um numero inteiro"
 
+#p - lista de pecas
 
-a={'identificacao':'carlos', 'movimentos':b}
+p=[]
 
-data_string=json.dumps(a)
-url = "http://127.0.0.1:5000/conjunto/new"
-p = requests.post(url, data=data_string)
+#Pecas para cada teste
+
+while (True):
+    try:
+        for i in range(l):
+            while (True):
+                x = int(raw_input(("Digite um numero de pecas maior que 0 e menor que 30. #%i:  ")%i))
+                if (x>0 and x<=30):
+                    break
+            p.append(x)
+        break
+    except ValueError:
+        print "Erro: Digite um numero de pecas maior que 0 e menor que 30:"
+print p
+
+#utilizando a funcao para cada item na lista de pecas
+
+j=[]
+for f in range(l):
+    n=hannoi(p[f],1,2,3)
+    j.append({'identificacao':'teste%i'%f,'movimentos':n})
+f={'contador':l,'testes':j}
+f_json=json.dumps(f)
+print f_json
