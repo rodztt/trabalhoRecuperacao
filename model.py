77883c742ddf4242164eafa0a7e23ca2f8a67675
Rodrigotanure <rodrigottricarico@gmail.com>
2015-07-22 23:05:42 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,13 @@
 from mauaserver import db
 
+class Conjuntos(db.Model):
+        id = db.Column(db.Integer, primary_key = True)
+        contador        = db.Column(db.Integer)
+        testes          = db.relationship('Testes',backref='conjuntos',
+                                lazy='dynamic')
+
 class Testes(db.Model):
 	id		= db.Column(db.Integer, primary_key=True)
-	contador        = db.Column(db.Integer)      
+	foreign_id          = db.Column(db.Integer, db.ForeignKey('conjuntos.id'))
 	identificacao	= db.Column(db.String(100))
 	movimentos	= db.Column(db.Integer)
